654f7d304604abb9939ca7c476da8fc8682f6dd3
Mohammed AlShannaq <m.shannaq@gmail.com>
2023-08-30 17:09:56 +0300

    customize

--- app/Config/Security.php
+++ app/Config/Security.php
@@ -33,7 +33,7 @@ class Security extends BaseConfig
      *
      * Token name for Cross Site Request Forgery protection.
      */
-    public string $tokenName = 'csrf_test_name';
+    public string $tokenName = 'csrf_smarty';
 
     /**
      * --------------------------------------------------------------------------
@@ -51,7 +51,7 @@ class Security extends BaseConfig
      *
      * Cookie name for Cross Site Request Forgery protection.
      */
-    public string $cookieName = 'csrf_cookie_name';
+    public string $cookieName = 'csrf_smarty_c';
 
     /**
      * --------------------------------------------------------------------------

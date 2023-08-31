5f96c99f2c57f6c8c7f834585840c9457de2580b
Mohammed AlShannaq <m.shannaq@gmail.com>
2023-08-31 18:10:28 +0300

    small auth config changes

--- app/Config/Auth.php
+++ app/Config/Auth.php
@@ -95,7 +95,7 @@ class Auth extends ShieldAuth
      */
     public array $redirects = [
         'register'    => '/',
-        'login'       => '/',
+        'login'       => '/dashboard',
         'logout'      => 'account/login',
         'force_reset' => '/',
     ];

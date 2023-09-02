68fa1d2c41b2277407de494f7a3fe8d12d5571b0
Mohammed AlShannaq <m.shannaq@gmail.com>
2023-09-02 13:48:54 +0300

    performance system localization

--- app/Config/Cookie.php
+++ app/Config/Cookie.php
@@ -45,7 +45,7 @@ class Cookie extends BaseConfig
      *
      * Set to `.your-domain.com` for site-wide cookies.
      */
-    public string $domain = '';
+    public string $domain = '.example.com';
 
     /**
      * --------------------------------------------------------------------------

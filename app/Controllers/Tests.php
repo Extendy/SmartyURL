bb5819dd0a5300c584c43a5567ade5f095b0eb45
Mohammed AlShannaq <m.shannaq@gmail.com>
2023-09-04 21:00:17 +0300

    adding Tests controller

---
+++ app/Controllers/Tests.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+/**
+ * Class BaseController
+ *
+ * Url Controller Deal with URL of SmartyURL
+ *
+ * For security be sure to declare any new methods as protected or private.
+ */
+class Tests extends BaseController
+{
+    public function index()
+    {
+        d('this is the index methos of tests controller');
+    }
+}

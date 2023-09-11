2a3a94cc4159bafb45bb33f8f02a618c16e3cc30
Mohammed AlShannaq <m.shannaq@gmail.com>
2023-09-11 16:07:40 +0300

    fix strings

--- app/Views/basic/users/changepwd.php
+++ app/Views/basic/users/changepwd.php
@@ -42,7 +42,7 @@
                         <div class="card-header">
                             <h3 class="card-title">
                                 <i class="fas fa-edit"></i>
-                                Modal Examples
+                                <?=lang('Account.changeActpassword');?>
                             </h3>
                         </div>
                         <div class="card-body">

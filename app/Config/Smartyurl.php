e68165952c17dd4209385d288cfaae0a9f58e1e0
Mohammed AlShannaq <m.shannaq@gmail.com>
2023-09-15 22:51:42 +0300

    add Smarty.php config support

--- app/Config/Smartyurl.php
+++ app/Config/Smartyurl.php
@@ -53,14 +53,6 @@ class Smartyurl extends BaseConfig
     ];
 
     /**
-     * @var string contain the file name of jquery supported version eg jquery-3.7.1 without js
-     *             for example if jquery-3.7.1 specified that mean there is a file with name
-     *             jquery-3.7.1.js and jquery-3.7.1.min.js in cdn/js/jquery/{]
-     *             if you don't show what you should use , keep it as default jquery-3.7.1
-     */
-    public $jquery_supported = 'jquery-3.7.1';
-
-    /**
      * --------------------------------------------------------------------
      * Customize the DB group used for each model
      * --------------------------------------------------------------------

264e49ebcf3788b5d1f409b92f657e646d669af4
Mohammed AlShannaq <m.shannaq@gmail.com>
2023-09-21 06:11:17 +0000

    adjust comments

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,25 +9,24 @@ $routes->get('/', 'Home::index');
 
 $routes->group('account', static function ($routes) {
     service('auth')->routes($routes);
-    $routes->get('changepwd', 'Account::changepwd',['filter' => 'session']);
-    $routes->post('changepwd', 'Account::changepwdAction',['filter' => 'session']);
+    $routes->get('changepwd', 'Account::changepwd', ['filter' => 'session']);
+    $routes->post('changepwd', 'Account::changepwdAction', ['filter' => 'session']);
 });
 
-$routes->get('/dashboard', 'Dashboard::index',['filter' => 'session']);
+$routes->get('/dashboard', 'Dashboard::index', ['filter' => 'session']);
 
 // URL
 $routes->group('url', static function ($routes) {
-    $routes->get('/', 'Url::index',['filter' => 'session']);
-    $routes->get('new', 'Url::new',['filter' => 'session']);
-    $routes->post('new', 'Url::newAction',['filter' => 'session']);
+    $routes->get('/', 'Url::index', ['filter' => 'session']);
+    $routes->get('new', 'Url::new', ['filter' => 'session']);
+    $routes->post('new', 'Url::newAction', ['filter' => 'session']);
 });
 
-//URL redirects
-#with go route
+// URL redirects
+// with go route
 $routes->get('go/(:any)', 'Go::go/$1');
-#Route any undefined request to Go Controller.
+// Route any undefined request to Go Controller.
 $routes->add('(:any)', 'Go::go/$1');
-
 
 // testing , @TODO must be removed after testing before any production release
 $routes->get('/url/none', 'Url::none');

970f90d17a46c347e1aeaf42ac8bc197d75c53b4
Mohammed AlShannaq <m.shannaq@gmail.com>
2023-10-15 16:46:00 +0000

    planning

---
+++ src/Url.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Extendy\Smartyurl;
+
+class Url
+{
+    /**
+     * This function delete the given $urlId from the database , $urlid can be single integer or array of urlids
+     *
+     * @return void
+     */
+    public function delUrl(int|array $urlId)
+    {
+        // @TODO @FIXME delUrl
+    }
+}

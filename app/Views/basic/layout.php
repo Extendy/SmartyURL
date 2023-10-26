9596f19a14ced456c1e8b159eee21e5265e945e6
Mohammed AlShannaq <m.shannaq@gmail.com>
2023-10-26 20:55:22 +0000

    fix: change sidebar urls bucket links

--- app/Views/basic/layout.php
+++ app/Views/basic/layout.php
@@ -9,23 +9,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="title" content="<?= $this->renderSection('title') ?>">
     <meta name="author" content="Extendy Team">
-    <meta name="description" content="AdminLTE is a Free Bootstrap 5 Admin Dashboard, 30 example pages using Vanilla JS.">
-    <meta name="keywords" content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, colorlibhq, colorlibhq dashboard, colorlibhq admin dashboard">
+    <meta name="description"
+          content="AdminLTE is a Free Bootstrap 5 Admin Dashboard, 30 example pages using Vanilla JS.">
+    <meta name="keywords"
+          content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, colorlibhq, colorlibhq dashboard, colorlibhq admin dashboard">
     <!--end::Primary Meta Tags-->
     <!--begin::Fonts-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,700;1,400&display=swap"
+          rel="stylesheet">
     <!--end::Fonts-->
     <!--begin::Third Party Plugin(OverlayScrollbars)-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.1.0/styles/overlayscrollbars.min.css" integrity="sha256-LWLZPJ7X1jJLI5OG5695qDemW1qQ7lNdbTfQ64ylbUY=" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.1.0/styles/overlayscrollbars.min.css"
+          integrity="sha256-LWLZPJ7X1jJLI5OG5695qDemW1qQ7lNdbTfQ64ylbUY=" crossorigin="anonymous">
     <!--end::Third Party Plugin(OverlayScrollbars)-->
     <!--begin::Third Party Plugin(Bootstrap Icons)-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css" integrity="sha256-BicZsQAhkGHIoR//IB2amPN5SrRb3fHB8tFsnqRAwnk=" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css"
+          integrity="sha256-BicZsQAhkGHIoR//IB2amPN5SrRb3fHB8tFsnqRAwnk=" crossorigin="anonymous">
     <!--end::Third Party Plugin(Bootstrap Icons)-->
     <!--begin::Required Plugin(AdminLTE)-->
-    <link rel="stylesheet" href="<?= smarty_cdn()?>css/adminlte<?= smarty_current_lang_dotdirection(); ?>.min.css">
-    <link rel="stylesheet" href="<?= smarty_cdn()?>css/smarty<?= smarty_current_lang_dotdirection(); ?>.css">
+    <link rel="stylesheet" href="<?= smarty_cdn() ?>css/adminlte<?= smarty_current_lang_dotdirection(); ?>.min.css">
+    <link rel="stylesheet" href="<?= smarty_cdn() ?>css/smarty<?= smarty_current_lang_dotdirection(); ?>.css">
     <!--end::Required Plugin(AdminLTE)-->
     <!--BEGIN: cssheaderarea css header area contains any specific javascript files that view files may load -->
     <?= $this->renderSection('cssheaderarea') ?>
@@ -53,12 +58,14 @@
 
 
                 <li class="nav-item d-none d-md-block">
-                    <a href="/url/new" class="btn btn-primary"><i class="bi bi-plus-lg"></i> <?= lang('Url.addNewURLMainbtn'); ?></a>
+                    <a href="/url/new" class="btn btn-primary"><i
+                            class="bi bi-plus-lg"></i> <?= lang('Url.addNewURLMainbtn'); ?></a>
                 </li>
 
                 <!--begin: +URL on small screen only -->
                 <li class="nav-item d-block d-sm-none d-md-none d-lg-none d-xl-none">
-                    <a href="/url/new" class="btn btn-primary"><i class="bi bi-plus-lg"></i> <?= lang('Url.addNewURLMainShortbtn'); ?></a>
+                    <a href="/url/new" class="btn btn-primary"><i
+                            class="bi bi-plus-lg"></i> <?= lang('Url.addNewURLMainShortbtn'); ?></a>
                 </li>
                 <!--end: +URL on small screen only -->
             </ul>
@@ -74,7 +81,6 @@
 
                 </li>
                 <!--end::Navbar Search-->
-
 
 
                 <!--begin::Notifications Dropdown Menu-->
@@ -105,19 +111,17 @@
                 <!--end::Notifications Dropdown Menu-->
 
 
-
                 <!--begin::User Menu Dropdown-->
                 <li class="nav-item dropdown user-menu">
                     <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
 
 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor"
+                             class="bi bi-person-circle" viewBox="0 0 16 16">
                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"></path>
-                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"></path>
+                            <path fill-rule="evenodd"
+                                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"></path>
                         </svg>
-
-
-
 
 
                     </a>
@@ -137,7 +141,8 @@
                                 <div class="col-12 text-center">
 
                                     <a class="btn btn-secondary btn-sm" href="<?= site_url('account/changepwd'); ?>">
-                                        <i class="bi bi-person-fill-gear"></i>  <?= lang('Common.accountSettingsLnk'); ?></a>
+                                        <i class="bi bi-person-fill-gear"></i> <?= lang('Common.accountSettingsLnk'); ?>
+                                    </a>
                                 </div>
 
 
@@ -148,7 +153,8 @@
                         <!--begin::Menu Footer-->
                         <li class="user-footer">
 
-                            <a href="/account/logout" class="btn btn-light float-end"><i class="bi bi-box-arrow-right"></i>
+                            <a href="/account/logout" class="btn btn-light float-end"><i
+                                    class="bi bi-box-arrow-right"></i>
                                 <?= lang('Common.accountLogoutLnk'); ?>
                             </a>
                         </li>
@@ -188,7 +194,8 @@
                 <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
 
                     <li class="nav-item">
-                        <a href="<?= site_url('/dashboard'); ?>" class="nav-link <?= (url_is('/dashboard')) ? 'active' : '' ?>">
+                        <a href="<?= site_url('/dashboard'); ?>"
+                           class="nav-link <?= (url_is('/dashboard')) ? 'active' : '' ?>">
                             <i class="nav-icon bi bi-house-door"></i>
                             <p>
                                 <?= lang('Common.dashboardLnk'); ?>
@@ -208,20 +215,25 @@
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="<?= site_url('/url/user/' . user_id()); ?>" class="nav-link <?= (url_is('/url/user/' . user_id())) ? 'active' : '' ?>">
+                                <a href="<?= site_url('/url/user/' . user_id()); ?>"
+                                   class="nav-link <?= (url_is('/url/user/' . user_id())) ? 'active' : '' ?>">
                                     <i class="nav-icon bi bi-person"></i>
                                     <p><?= lang('Url.urlsMyLink'); ?></p>
                                 </a>
                             </li>
-                            <li class="nav-item">
-                                <a href="<?= site_url('/url'); ?>" class="nav-link <?= (url_is('/url/')) ? 'active' : '' ?>">
-                                    <i class="nav-icon bi bi-people"></i>
-                                    <p><?= lang('Url.urlsAllLink'); ?></p>
-                                </a>
-                            </li>
+                            <?php
+                            if (auth()->user()->can('admin.manageotherurls', 'super.admin')) {
+                                ?>
+                                <li class="nav-item">
+                                    <a href="<?= site_url('/url'); ?>"
+                                       class="nav-link <?= (url_is('/url/')) ? 'active' : '' ?>">
+                                        <i class="nav-icon bi bi-people"></i>
+                                        <p><?= lang('Url.urlsAllLink'); ?></p>
+                                    </a>
+                                </li>
+                            <?php } ?>
                         </ul>
                     </li>
-
 
 
                 </ul>
@@ -242,7 +254,10 @@
     <!--begin::Footer-->
     <footer class="app-footer">
         <!--begin::To the end-->
-        <div class="float-end d-none d-sm-inline"><?= config('Smarty')->smarty_name; ?>  <a target="_blank" rel="nofollow" href="<?= config('Smarty')->smarty_online_repo; ?>">v<?= config('Smarty')->smarty_version; ?></a></div>
+        <div class="float-end d-none d-sm-inline"><?= config('Smarty')->smarty_name; ?> <a target="_blank"
+                                                                                           rel="nofollow"
+                                                                                           href="<?= config('Smarty')->smarty_online_repo; ?>">v<?= config('Smarty')->smarty_version; ?></a>
+        </div>
         <!--end::To the end-->
         <!--begin::Copyright-->
         <strong>
@@ -257,13 +272,18 @@
 <!--end::App Wrapper-->
 <!--begin::Script-->
 <!--begin::Third Party Plugin(OverlayScrollbars)-->
-<script src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.1.0/browser/overlayscrollbars.browser.es6.min.js" integrity="sha256-NRZchBuHZWSXldqrtAOeCZpucH/1n1ToJ3C8mSK95NU=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.1.0/browser/overlayscrollbars.browser.es6.min.js"
+        integrity="sha256-NRZchBuHZWSXldqrtAOeCZpucH/1n1ToJ3C8mSK95NU=" crossorigin="anonymous"></script>
 <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+        integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
+        crossorigin="anonymous"></script>
 <!--end::Required Plugin(popperjs for Bootstrap 5)--><!--begin::Required Plugin(Bootstrap 5)-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
+        integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
+        crossorigin="anonymous"></script>
 <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
-<script src="<?= smarty_cdn()?>js/adminlte.js"></script>
+<script src="<?= smarty_cdn() ?>js/adminlte.js"></script>
 <script src="<?= smarty_include_jquery(); ?>"></script>
 <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
 <script>
@@ -274,7 +294,7 @@
         scrollbarClickScroll: true,
     };
 
-    document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("DOMContentLoaded", function () {
         const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
         if (
             sidebarWrapper &&

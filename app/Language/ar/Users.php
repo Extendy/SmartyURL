c13308b571ac3a8f9b129dd9d5a83a429e3e7de8
Mohammed AlShannaq <m.shannaq@gmail.com>
2023-11-23 19:04:42 +0000

    lang: add list user lang strings

--- app/Language/ar/Users.php
+++ app/Language/ar/Users.php
@@ -1,5 +1,17 @@
 <?php
 
 return [
-    'UserNotFound' => 'مستخدم غير موجود.',
+    'UserNotFound'    => 'مستخدم غير موجود.',
+    'SystemUserMenu'  => 'مستخدمي النظام',
+    'SystemUsersList' => 'استعراض المستخدمين',
+
+    'ListUsersColId'             => 'رقم',
+    'ListUsersColUsername'       => 'اسم المستخدم',
+    'ListUsersColEmail'          => 'البريد',
+    'ListUsersColUserGroup'      => 'مجموعة المستخدمين',
+    'ListUsersColUserLastActive' => 'اخر نشاط',
+    'ListUsersColUserUrlCount'   => 'عدد روابطة',
+    'ListUsersColUserActions'    => 'اجراءات',
+
+    'UsersAddNewUser' => 'اضافة مستخدم جديد',
 ];
--- app/Language/en/Users.php
+++ app/Language/en/Users.php
@@ -1,5 +1,17 @@
 <?php
 
 return [
-    'UserNotFound' => 'User Not found.',
+    'UserNotFound'    => 'User Not found.',
+    'SystemUserMenu'  => 'System Users',
+    'SystemUsersList' => 'List Users',
+
+    'ListUsersColId'             => 'ID',
+    'ListUsersColUsername'       => 'Username',
+    'ListUsersColEmail'          => 'Email',
+    'ListUsersColUserGroup'      => 'User Group',
+    'ListUsersColUserLastActive' => 'Last Active',
+    'ListUsersColUserUrlCount'   => 'URLs Count',
+    'ListUsersColUserActions'    => 'Actions',
+
+    'UsersAddNewUser' => 'Add New User',
 ];

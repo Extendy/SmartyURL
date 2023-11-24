cd03de55faeed63ec0acfbf9a1e7164c16f7c01d
Mohammed AlShannaq <m.shannaq@gmail.com>
2023-11-24 20:42:37 +0000

    lang: add users list lang strings

--- app/Language/ar/Users.php
+++ app/Language/ar/Users.php
@@ -5,13 +5,19 @@ return [
     'SystemUserMenu'  => 'مستخدمي النظام',
     'SystemUsersList' => 'استعراض المستخدمين',
 
-    'ListUsersColId'             => 'رقم',
-    'ListUsersColUsername'       => 'اسم المستخدم',
-    'ListUsersColEmail'          => 'البريد',
-    'ListUsersColUserGroup'      => 'مجموعة المستخدمين',
-    'ListUsersColUserLastActive' => 'اخر نشاط',
-    'ListUsersColUserUrlCount'   => 'عدد روابطة',
-    'ListUsersColUserActions'    => 'اجراءات',
+    'ListUsersColId'                  => 'رقم',
+    'ListUsersColUsername'            => 'اسم المستخدم',
+    'ListUsersColEmail'               => 'البريد',
+    'ListUsersColEmailVerifiedStatus' => 'تاكيد البريد',
+    'ListUsersColUserGroup'           => 'مجموعة المستخدمين',
+    'ListUsersColUserLastActive'      => 'اخر نشاط',
+    'ListUsersColUserUrlCount'        => 'عدد روابطة',
+    'ListUsersColUserActions'         => 'اجراءات',
+
+    'ListUsersEmailVerifiedStatusActiveYes'  => 'مؤكد',
+    'ListUsersEmailVerifiedStatusActiveNo'   => 'غير مؤكد',
+    'ListUsersEmailVerifiedStatusActivate'   => 'تاكيد',
+    'ListUsersEmailVerifiedStatusDeActivate' => 'الغاء تاكيد',
 
     'UsersAddNewUser' => 'اضافة مستخدم جديد',
 ];
--- app/Language/en/Users.php
+++ app/Language/en/Users.php
@@ -5,13 +5,19 @@ return [
     'SystemUserMenu'  => 'System Users',
     'SystemUsersList' => 'List Users',
 
-    'ListUsersColId'             => 'ID',
-    'ListUsersColUsername'       => 'Username',
-    'ListUsersColEmail'          => 'Email',
-    'ListUsersColUserGroup'      => 'User Group',
-    'ListUsersColUserLastActive' => 'Last Active',
-    'ListUsersColUserUrlCount'   => 'URLs Count',
-    'ListUsersColUserActions'    => 'Actions',
+    'ListUsersColId'                  => 'ID',
+    'ListUsersColUsername'            => 'Username',
+    'ListUsersColEmail'               => 'Email',
+    'ListUsersColEmailVerifiedStatus' => 'Email Verified',
+    'ListUsersColUserGroup'           => 'User Group',
+    'ListUsersColUserLastActive'      => 'Last Active',
+    'ListUsersColUserUrlCount'        => 'URLs Count',
+    'ListUsersColUserActions'         => 'Actions',
+
+    'ListUsersEmailVerifiedStatusActiveYes'  => 'Verified',
+    'ListUsersEmailVerifiedStatusActiveNo'   => 'Not Verified',
+    'ListUsersEmailVerifiedStatusActivate'   => 'Activate',
+    'ListUsersEmailVerifiedStatusDeActivate' => 'Deactivate',
 
     'UsersAddNewUser' => 'Add New User',
 ];

2ae382ca1aa4f8e4adb8ba886b7a2bf2e1b0eae2
Mohammed AlShannaq <m.shannaq@gmail.com>
2023-12-27 14:19:22 +0300

    style: remove unused UI elements

--- app/Views/basic/layout.php
+++ app/Views/basic/layout.php
@@ -78,42 +78,7 @@
 
             <!--begin::End Navbar Links-->
             <ul class="navbar-nav ms-auto">
-                <!--begin::Navbar Search-->
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                        <i class="bi bi-search"></i>
-                    </a>
 
-                </li>
-                <!--end::Navbar Search-->
-
-
-                <!--begin::Notifications Dropdown Menu-->
-                <li class="nav-item dropdown">
-                    <a class="nav-link" data-bs-toggle="dropdown" href="#">
-                        <i class="bi bi-bell-fill"></i>
-                        <span class="navbar-badge badge text-bg-warning">15</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
-                        <span class="dropdown-item dropdown-header">15 Notifications</span>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="bi bi-envelope me-2"></i> 4 new messages
-                            <span class="float-end text-secondary fs-7">3 mins</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="bi bi-people-fill me-2"></i> 8 friend requests
-                            <span class="float-end text-secondary fs-7">12 hours</span>
-                        </a>
-
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item dropdown-footer">
-                            See All Notifications
-                        </a>
-                    </div>
-                </li>
-                <!--end::Notifications Dropdown Menu-->
 
 
                 <!--begin::User Menu Dropdown-->
